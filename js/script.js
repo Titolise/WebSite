cfcc8a83d76071d9ec7d5e9f7c4938851ad45705
Gianfilippo <gianfix01@gmail.com>
2025-07-18 16:23:08 +0200

    CSS update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-/*const button = document.getElementById('change_color');
-const body = document.body;
-
-button.addEventListener('click', () => {
-    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    body.style.backgroundColor = randomColor;
-});*/
+function toggleMenu(){
+    const menu = document.querySelector(".hamburger-links");
+    const icon = document.querySelector(".hamburger.icon");
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
